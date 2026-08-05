2662a1ccbc96740e519c8dcd49024e157c53c86b
Emmanuel <okoosiemmanuel@gmail.com>
2026-08-05 11:57:58 +0100

    feat: set up auth feat

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-
-const server = express();
+// import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js";
 
 const PORT = ENV.PORT || 3000;
 
-server.use("/api/auth", authRoutes);
-server.use("/api/messages", messageRoutes);
+const server = express();
 
-server.listen(3000, () => {
+server.use(express.json())
+
+server.use("/api/auth", authRoutes);
+// server.use("/api/messages", messageRoutes);
+
+server.get("/api/health", (req, res) => res.send("API Running"))
+server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
+  connectDB();
 });
