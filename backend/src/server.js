fcf05b088c3f9de552dd50a45eb578da5754a0d2
Emmanuel <okoosiemmanuel@gmail.com>
2026-08-06 00:49:44 +0100

    feat: implement messaging functionality with user contact retrieval and message sending

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { ENV } from "./lib/env.js";
 import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 
@@ -15,7 +15,7 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/api/health", (req, res) => res.send("API Running"));
 app.listen(PORT, () => {
